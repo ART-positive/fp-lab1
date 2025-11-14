c701357d52d68b3b9b3e3572e6504fe1202fdde4
ARTpositive <72326054+ART-positive@users.noreply.github.com>
2025-11-14 19:23:44 +0300

    style: naming str

--- test/my_app/euler08_test.clj
+++ test/my_app/euler08_test.clj
@@ -5,7 +5,7 @@
                                  tailrec
                                  mapindexed
                                  lazy
-                                 str]]))
+                                 str-input]]))
 
 (deftest a-test
   (testing "FIXME, I fail."
@@ -18,19 +18,19 @@
 
 (deftest sample-4-test
   (testing "Пример из условия: максимальное произведение 4 подряд цифр = 5832"
-    ;; используем str и проверяем, что для окна 4 получаем 5832
-    (is (= 5832 (recursive str 4)))
-    (is (= 5832 (tailrec str 4)))
-    (is (= 5832 (mapindexed str 4)))
-    (is (= 5832 (lazy str 4)))))
+    ;; используем str-input и проверяем, что для окна 4 получаем 5832
+    (is (= 5832 (recursive str-input 4)))
+    (is (= 5832 (tailrec str-input 4)))
+    (is (= 5832 (mapindexed str-input 4)))
+    (is (= 5832 (lazy str-input 4)))))
 
 (deftest full-13-test
   (testing "Полный 1000-значный пример: ожидаем 23514624000"
     (let [expected 23514624000]
-      (is (= expected (recursive str 13)))
-      (is (= expected (tailrec str 13)))
-      (is (= expected (mapindexed str 13)))
-      (is (= expected (lazy str 13))))))
+      (is (= expected (recursive str-input 13)))
+      (is (= expected (tailrec str-input 13)))
+      (is (= expected (mapindexed str-input 13)))
+      (is (= expected (lazy str-input 13))))))
 
 (deftest short-input-test
   (testing "Если длина входа меньше окна, возвращаем 0"
